3b21cc29896b86b3cc962c4e50807eaee1d34bf3
Natasha Matthews <natashamatthews90@gmail.com>
2022-02-01 19:49:49 -0600

    did the exercie

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -25,17 +25,50 @@ public class HelloController {
     //Handles request of the form /hello?name=LaunchCode
     //@GetMapping("hello")
     //@RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
-    public String helloWithQueryParam(@RequestParam  String name ) {
-        return "Hello, " + name + "!";
-    }
+    //@RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
+    //public String helloWithQueryParam(@RequestParam  String name ) {
+       // return "Hello, " + name + "!";
+    //}
 
 
     //handles request of the form /hello/LaunchCode
     //@GetMapping("hello/{name}")
-    @GetMapping("{name}")
-    public String helloWithPathParam(@PathVariable String name) {
-        return "Hello, " + name + "!";
+    //@GetMapping("{name}")
+   // public String helloWithPathParam(@PathVariable String name) {
+       // return "Hello, " + name + "!";
+    //}
+
+    @RequestMapping(method = RequestMethod.POST)
+    @ResponseBody
+    public String helloPost(@RequestParam String name, @RequestParam String language) {
+        if (name == null) {
+            name = "World";
+        }
+        return createMessage(name, language);
+
+    }
+
+
+    public static String createMessage(String n, String l) {
+        String greeting = "";
+
+        if (l.equals("english")) {
+            greeting = "Hello";
+        }
+        else if (l.equals("french")) {
+            greeting = "Bonjour";
+        }
+        else if (l.equals("italian")) {
+            greeting = "Bonjourno";
+        }
+        else if (l.equals("spanish")) {
+            greeting = "Hola";
+        }
+        else if (l.equals("german")) {
+            greeting = "Hallo";
+        }
+
+        return greeting + " " + n;
     }
 
     // live at /hello/form
@@ -46,6 +79,13 @@ public class HelloController {
                         "<body>" +
                         "<form method = 'post' action = '/hello'>" +
                         "<input type = 'text' name = 'name' />" +
+                        "<select name='language'>" +
+                            "<option value='english'>English</option>" +
+                            "<option value='french' >French</option>" +
+                            "<option value='spanish'>Spanish</option>" +
+                            "<option value='italian'>Italian</option>" +
+                            "<option value='german'>German</option>" +
+                        "</select>" +
                         "<input type = 'submit' value = 'Greet Me!' />" +
                         "</form>" +
                         "</body>" +
